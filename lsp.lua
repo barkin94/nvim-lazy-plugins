6fb502778bc295b39a028082f6e86325a7ece3f3
barkin <barkinsagin@gmail.com>
2024-12-01 22:22:56 +0300

    add lsp plugins

---
+++ lsp.lua
@@ -0,0 +1,37 @@
+return {
+  -- -- In order to modify the `lspconfig` configuration:
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --    config = function()
+  --       require "configs.lspconfig"
+  --    end,
+  -- },
+  --
+
+  -- Mason LSPconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    lazy = false,
+    event = "VeryLazy",
+    -- opts = {
+    --     automatic_installation = true,
+    --   }
+    config = function()
+      require("mason-lspconfig").setup()
+      require("mason-lspconfig").setup_handlers {
+          -- The first entry (without a key) will be the default handler
+          -- and will be called for each installed server that doesn't have
+          -- a dedicated handler.
+          function (server_name) -- default handler (optional)
+              require("lspconfig")[server_name].setup {}
+          end,
+          -- Next, you can provide a dedicated handler for specific servers.
+          -- For example, a handler override for the `rust_analyzer`:
+          ["rust_analyzer"] = function ()
+              require("rust-tools").setup {}
+          end
+      }
+    end,
+  },
+}
